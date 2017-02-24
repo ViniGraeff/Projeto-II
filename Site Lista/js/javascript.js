2a56168b69f9469c9c69923bf1a2d1ec6ea57920
Vinícius Graeff Sturmer <vinicius.sturmer@digitaldesk.com.br>
2017-02-24 15:57:05 -0300

    Trocando de js pra Jquery

--- Site Lista/js/javascript.js
+++ Site Lista/js/javascript.js
@@ -1,14 +1,16 @@
-var text, aux, str, NOME, VALOR, STATUS, ESTOQUE, flag=0, flag2=0, igual=0, del;
+var text, NOME, VALOR, STATUS, ESTOQUE, flag=0, flag2=0, igual=0, del;
+//text tem que ser global
+
 
 var servidor="http://192.168.1.172:3000/product";
 
-print = function(){
+print = function(){//Função que printa a tabela
 	$('#table').empty();
 	$.get(servidor, function(data) {
 		text=data;
 		for(var i=0;i<text.length;i++){
-			aux=text[i].id;
-			str=text[i].valor.toString();
+			var aux=text[i].id;
+			var str=text[i].valor.toString();
 			str=parseFloat(str).toFixed(2);
 			str=str.replace(".",",");
 			if(flag==0){
@@ -25,25 +27,35 @@ print = function(){
 	});
 	console.log("printou");
 	$('#alerta').hide();
-}
+}//Fim da função que printa a tabela
 
-save = function(){
+save = function(){//Função que salva o conteúdo dos inputs em variáveis
 	NOME = $('#nome').val();
 	VALOR = $('#valor').val();
 	STATUS = $('#status').val();
 	ESTOQUE = $('#estoque').val();
-}
+}//Fim da função que salva os inputs
 
-varredura = function(){
+adiciona = function (){//Função qe verifica se todos os campos estão preenchidos
+	save();
+	if(NOME=="" || VALOR=="" || STATUS=="" || ESTOQUE==""){
+		timerAlert();
+		$('#textalert').html("Todos os campos devem estar preenchidos!");
+	}else{
+		varredura();
+	}
+}//Fim da função que verifica se os campos estão preenchidos
+
+varredura = function(){//Função que faz a varredura dos nomes, pra ver se já existe algum igual
 	for(var i=0;i<text.length;i++){
 		if(NOME.toLowerCase()===text[i].nome.toLowerCase()){
 			igual=1;
 		}
 	}
 	confereIgual();
-}
+}//Fim da função de varredura
 
-confereIgual = function(){
+confereIgual = function(){//Função que é executada após a varredura, confere se foi achado algum item igual ao que será adicionado
 	if(igual==0){
 		$.ajax({
 			type: 'POST',
@@ -66,19 +78,9 @@ confereIgual = function(){
 		$('#textalert').html("Item já existente!");
 		igual=0;
 	}
-}
+}//Fim da função que confere se há itens iguais
 
-adiciona = function (){
-	save();
-	if(NOME=="" || VALOR=="" || STATUS=="" || ESTOQUE==""){
-		timerAlert();
-		$('#textalert').html("Todos os campos devem estar preenchidos!");
-	}else{
-		varredura();
-	}
-}
-
-edita = function(){ 
+edita = function(){ //Função que verifica se todos os campos estão preenchidos e edita o item desejado
 	save();
 	if(NOME=="" || VALOR=="" || STATUS=="" || ESTOQUE==""){
 		timerAlert();
@@ -101,18 +103,18 @@ edita = function(){
 		timerAlert3();
 		$('#textalert3').html("Item editado com sucesso!");
 	}
-}
+}//Fim da função que edita
 
-timerAlert = function(){
+timerAlert = function(){//Função que joga no modal um alerta, caso não estejam preenchidos todos os campos ou caso o item já exista
 	$('#alerta').show();
 	window.setTimeout(function() {
     $(".alert").slideUp(500, function(){
         $(this).hide(); 
     });
 }, 4000);
-}
+}//Fim da função de alerta1
 
-timerAlert2 = function(){
+timerAlert2 = function(){//Função que joga um alerta na página principal, avisando que ocorreu algum erro com a ação feita
 	$('#alerta2').show();
 	$('#textalert2').html("Ops! Ocorreu algum problema...");
 	window.setTimeout(function() {
@@ -121,18 +123,18 @@ timerAlert2 = function(){
     });
 }, 4000);
 	print();
-}
+}//Fim da função de alerta2
 
-timerAlert3 = function(){
+timerAlert3 = function(){//Função que joga um alerta na página principal, indicando que as alterações foram feitas com sucesso
 	$('#alerta3').show();
 	window.setTimeout(function() {
     $(".alert").slideUp(500, function(){
         $(this).hide(); 
     });
 }, 4000);
-}
+}//Fim da função de alerta3
 
-noPaste = function(){
+noPaste = function(){//Função que impede CTRL+C e CTRL+V nas caixas de texto
 	$('#nome').bind("cut copy paste",function(e) {
 	e.preventDefault();
 	});
@@ -144,17 +146,17 @@ noPaste = function(){
 	$('#estoque').bind("cut copy paste",function(e) {
 	e.preventDefault();
 	});
-}
+}//Fim da função CTRL+C/CTRL+V
 
-deleta = function(x){
+deleta = function(x){//Função que arruma o modal do botão de delete
 	del = x;
 	flag2=2;
 	tituloModal();
 	hideButton();
 	$('.textBox').hide();
-}
+}//fim da função modal delete
 
-deletaConfirma = function(){
+deletaConfirma = function(){//Função que é executada ao confirmar que se quer deletar um item no modal de delete
 	$.ajax({
 		type: 'DELETE',
 		url: servidor +"/"+ del,
@@ -163,40 +165,40 @@ deletaConfirma = function(){
 		
 	});
 	$('#abrir').modal('hide');
-}
+}//Fim da função de confirmar o delete
 
-preencher = function(z){
+preencher = function(z){//Função que prepara o modal de editar, preenchendo as caixas de texto com as informações do item selecionado.
 	$('#alerta').hide();
 	$('.textBox').show();
-	document.getElementById('nome').value = text[z].nome;
-	document.getElementById('valor').value = text[z].valor;
-	document.getElementById('status').value = text[z].status;
-	document.getElementById('estoque').value = text[z].estoque;
+	$('#nome').val(text[z].nome);
+	$('#valor').val(text[z].valor);
+	$('#status').val(text[z].status);
+	$('#estoque').val(text[z].estoque);
 	ID = text[z].id;
 	flag2 = 1;
 	tituloModal();
 	hideButton();
-}
+}//Fim da função do modal editar
 
-mudarTitulo = function(){
+mudarTitulo = function(){//Função que muda o título da página
 	if(flag==0){
-		document.getElementById('titulo').innerHTML = "Lista de Itens Ativos";
+		$('#titulo').html("Lista de Itens Ativos");
 	}else{
-		document.getElementById('titulo').innerHTML = "Lista de Itens Inativos";
+		$('#titulo').html("Lista de Itens Inativos");
 	}
-}
+}//Fim da função título página
 
-tituloModal = function(){
+tituloModal = function(){//Função que muda o título do modal
 	if(flag2==0){
-		document.getElementById('tituloModal').innerHTML = "Adicionar Itens";
+		$('#tituloModal').html("Adicionar Itens");
 	}else if(flag2==1){
-		document.getElementById('tituloModal').innerHTML = "Editar Itens";
+		$('#tituloModal').html("Editar Itens");
 	}else{
-		document.getElementById('tituloModal').innerHTML = "Tem certeza que deseja excluir este item?";
+		$('#tituloModal').html("Tem certeza que deseja excluir este item?");
 	}
-}
+}//Fim da função título modal
 
-hideButton = function(){
+hideButton = function(){//Função que prepara os botões do modal, dependendo da função que se quer realizar
 	if(flag2==0){
 	$("#adiciona").show();
 	$("#editar").hide();
@@ -210,19 +212,19 @@ hideButton = function(){
 	$("#editar").hide();
 	$("#botdel").show();
 	}
-}
+}//Fim da função dos botões
 
-mascara = function(){
+mascara = function(){//Função que adiciona uma máscara no campo "valor"
 	$("#valor").maskMoney({showSymbol:true, symbol:"R$", decimal:".", thousands:""});
-}
+}//Fim da máscara
 
-condicoes = function(){
+condicoes = function(){//Função que cuida das restrições de caracteres para cada caixa de texto
 	$('#nome').keypress(isNumberKey1);
 	$('#valor').keypress(isNumberKey);
 	$('#estoque').keypress(isNumberKey2);
-}
+}//Fim da função das restrições
 
-actions = function(){
+actions = function(){//Função que cuida de todas as ações
 	condicoes();
 
 	$("#modalclean").click(function(){
@@ -263,7 +265,7 @@ actions = function(){
 	});
 
     $('[data-toggle="tooltip"]').tooltip(); 
-}
+}//Fim da função que cuida das ações
 
 function isNumberKey(evt)
 {
